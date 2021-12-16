bba85152f56d647a54ceab4fe05ce87a046ed58a
AndreyPimkin <pimkin03@bk.ru>
2021-12-16 17:14:44 +0300

    Написал импорты, убрал строку заголовка, сделал полный экран

--- app/src/main/java/com/example/topteam/MainActivity.java
+++ app/src/main/java/com/example/topteam/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.topteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }

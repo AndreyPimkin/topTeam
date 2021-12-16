7afbc8b4d65cb89417dd5e162c2e4629444b9765
Zzzhukov <valentin.zhukov.72@mail.ru>
2021-12-16 17:26:13 +0300

    Создал второе активити и связал его с первым

---
+++ app/src/main/java/com/example/topteam/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.topteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class SecondActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_two);
+        TextView textView = findViewById(R.id.teamName);
+}
